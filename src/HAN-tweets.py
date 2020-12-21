849fb28ca266959f8dc592516c4306b8a7e317af
uyplayer <32848979@qq.com>
2020-12-21 16:32:02 +0800

    okk

--- src/HAN-tweets.py
+++ src/HAN-tweets.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2020/12/16 19:48
+# @Time    : 2020/12/18 15:32
 # @Author  : uyplayer
 # @Site    : uyplayer.pw
-# @File    : TextAttBiRNN-tweets.py
+# @File    : HAN-tweets.py.py
 # @Software: PyCharm
+
+# https://github.com/ShawnyXiao/TextClassification-Keras#6-han
 
 # dependency library
 from __future__ import print_function
@@ -43,28 +45,26 @@ from keras.preprocessing.sequence import pad_sequences
 # pre_tools
 from pre_tools.load_data_tweets import return_data
 # warnings
-import warnings
-warnings.filterwarnings("ignore")
-
-# https://www.kaggle.com/mlwhiz/attention-pytorch-and-keras
+# import warnings
+# warnings.filterwarnings("ignore")
 
 # PARAMS
 # WORD2VEC
-W2V_SIZE = 300
 W2V_WINDOW = 7
 W2V_EPOCH = 32
 W2V_MIN_COUNT = 10
 # KERAS
 SEQUENCE_LENGTH = 300
 EPOCHS = 20
-BATCH_SIZE = 1024
+BATCH_SIZE = 200
 # SENTIMENT
 POSITIVE = "POSITIVE"
 NEGATIVE = "NEGATIVE"
 NEUTRAL = "NEUTRAL"
 SENTIMENT_THRESHOLDS = (0.4, 0.7)
+
 # MODEL PATH
-MODEL_PATH = "../model_files/Sentiment140 dataset with 1.6 million tweets/TextAttBiRNN-tweets.pth"
+MODEL_PATH = "../model_files/Sentiment140 dataset with 1.6 million tweets/HAN-tweets.pth"
 
 # GPU check
 # device = torch.device("cpu")
@@ -108,6 +108,8 @@ vocab_size = len(tokenizer.word_index) + 1
 x_train = pad_sequences(tokenizer.texts_to_sequences(train_x), maxlen=SEQUENCE_LENGTH) # 300  word to index
 x_test = pad_sequences(tokenizer.texts_to_sequences(test_x), maxlen=SEQUENCE_LENGTH) # 300
 
+# vocab_size
+vocab_size = len(tokenizer.word_index) + 1
 # Label Encoder ; process labels
 labels = train_y.unique().tolist()
 
@@ -146,72 +148,122 @@ valid_loader = DataLoader(valid_data, shuffle=True, batch_size=2)
 
 # my model
 class Model(nn.Module):
-
-    def __init__(self, input_size,hidden_dim,LSTM_layers,vocab_size):
+    def __init__(self,num_embeddings,embedding_dim,input_size,hidden_size,batch_first=True,bidirectional=True):
         super(Model, self).__init__()
 
-        # Hyperparameters
-        self.hidden_dim = hidden_dim
-        self.LSTM_layers = LSTM_layers
+        # params
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
         self.input_size = input_size
-        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.bidirectional = bidirectional
+        self.batch_first = batch_first
+        in_features = 600
+        out_features = 600
 
-        self.dropout = nn.Dropout(0.5)
-        self.embedding = nn.Embedding(self.vocab_size , self.hidden_dim, padding_idx=0)
-        self.lstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_dim, num_layers=self.LSTM_layers,
-                            batch_first=True)
-        self.fc1 = nn.Linear(in_features=self.hidden_dim, out_features=1)
-        self.att = nn.Linear(in_features=self.hidden_dim, out_features=1)
-        self.sig = nn.Sigmoid()
+        #embedding
+        self.embedding = nn.Embedding(self.num_embeddings, self.embedding_dim, padding_idx=0)
 
-    # attention
-    def attention(self,hidden):
-        # hidden : [1, 300, 300] ; out[:, -1, :] : [1, 300]
-        a = self.att(hidden)
-        out = self.sig(a)
+        # Forward and backward GRU ; word encode
+        self.biGRU = nn.GRU(input_size=self.input_size, hidden_size=self.hidden_size, batch_first=True,
+                            bidirectional=self.bidirectional)
+
+        # word attention
+        self.word_atten = nn.Linear(in_features=in_features,out_features=out_features)
+        self.w_sf = nn.Softmax(dim=1)
+
+        # Sentence Encoder
+        self.sentGRU = nn.GRU(input_size=600, hidden_size=1, batch_first=True,
+                              bidirectional=self.bidirectional)
+
+        # Sentence attention
+        self.sent_atten = nn.Linear(in_features=in_features, out_features=out_features)
+        self.s_sf = nn.Softmax(dim=1)
+
+        # Document Classification
+        self.doc = nn.Softmax(dim=2)
+        self.out = nn.Linear(in_features=2, out_features=1)
+
+    # word attention
+    def word_attention(self, hidden):
+        u = self.word_atten(hidden)
+        a = self.w_sf(u)
+        s = a*hidden
+        return s
+
+    # sentence attention
+    def sentence_attention(self, hidden):
+        u = self.sent_atten(hidden)
+        a = self.s_sf(u)
+        v = a * hidden
+        return v
+
+    def forward(self, text):
+
+        text = text.clone().detach()
+        text = text.to(device).long()
+
+        # word encoding x = W_e * w_it
+        embed = self.embedding(text)
+        w_output, w_hn = self.biGRU(embed)
+
+        # word attention
+        s = self.word_attention(w_output)
+
+        # Sentence attention
+        s_output, s_hn = self.sentGRU(s)
+
+        # doc
+        y = self.doc(s_output)
+        out = self.out(y) # (100,300,1)
+        out = torch.sum(out, 1)
         return out
-    def forward(self, x):
-        # Hidden and cell state definion
-        h = torch.zeros((self.LSTM_layers, x.size(0), self.hidden_dim)).to(device)
-        c = torch.zeros((self.LSTM_layers, x.size(0), self.hidden_dim)).to(device)
 
-        # Initialization fo hidden and cell states
-        torch.nn.init.xavier_normal_(h)
-        torch.nn.init.xavier_normal_(c)
-        # Each sequence "x" is passed through an embedding layer
-        x = x.clone().detach()
-        x = x.to(device).long()
-        out = self.embedding(x)
-        # Feed LSTMs
-        out, (hidden, cell) = self.lstm(out, (h, c)) # out[:, -1, :] : [1, 300]
-        a = self.attention(out)
-        c = a * out # [1, 300, 1]
-        return self.fc1(c[:, -1, :])  # [1, 1, 300] * [300,1] = [1, 1, 1]
 
+# model
+# num_embeddings = vocab_size
+# embedding_dim = 300
 # input_size = 300
-# hidden_dim = 300
-# LSTM_layers = 1
-# batch_size = 1
-# vocab_size = vocab_size
-#
-# model = Model(input_size = input_size,hidden_dim = hidden_dim ,LSTM_layers = LSTM_layers,batch_size = batch_size,
-#               vocab_size = vocab_size)
+# hidden_size = 300
+# batch_first = True
+# bidirectional = True
+
+# # model
+# model = Model(num_embeddings=num_embeddings, embedding_dim=embedding_dim, input_size=input_size,
+#               hidden_size=hidden_size,
+#               batch_first=batch_first,
+#               bidirectional=bidirectional)
 # model.to(device)
-# input = torch.randint(1,100, (1,300))
+#
+# # model summary
+# # summary(model,input_size=(300),batch_size=1)
+#
+#
+# # input
+# input = torch.randint(1, 1000, (1, 300))
+#
+# # output
 # out = model(input)
+# print(out.shape)
+# print(out)
+
 
 # training
 def train():
 
-    vocab_size = len(tokenizer.word_index) + 1
+    num_embeddings = len(tokenizer.word_index) + 1
+    embedding_dim = 300
     input_size = 300
     hidden_size = 300
-    num_layers = 1
+    batch_first = True
+    bidirectional = True
     epochs = EPOCHS
-    # epochs = 1
 
-    # model  vocab_size,hidden_dim,input_size,hidden_size,num_layers=2,bidirectional=True
-    model = Model(input_size = input_size,hidden_dim = hidden_size ,LSTM_layers = num_layers,vocab_size = vocab_size)
+    vocab_size = len(tokenizer.word_index) + 1
+    model = Model(num_embeddings=num_embeddings, embedding_dim=embedding_dim, input_size=input_size,
+                  hidden_size=hidden_size,
+                  batch_first=batch_first,
+                  bidirectional=bidirectional)
 
     # device
     model = model.to(device)
@@ -243,6 +295,7 @@ def train():
 
             # output
             output = model(train_x)
+
             # change demintion
             train_y = train_y.view(-1, 1)
 
@@ -263,9 +316,9 @@ def train():
                                                                                               accuracy, elapsed))
 
             # each 20
-            if i == len(train_loader)-1:
-                print("[%d, %5d] epoch_loss : %.3f  epoch_accuracy : %.3f" % ((epoch + 1, i + 1, running_loss / len(train_loader),
-                                                                               running_accuracy/len(train_loader))))
+            if i == 20:
+                print("[%d, %5d] epoch_loss : %.3f  epoch_accuracy : %.3f" % ((epoch + 1, i + 1, running_loss / 20,
+                                                                               running_accuracy/20)))
                 running_loss = 0.0
                 running_accuracy =0.0
 
@@ -307,12 +360,17 @@ def train():
 # evaluate
 def evaluate(text_list):
     vocab_size = len(tokenizer.word_index) + 1
+    hidden_dim = 300
     input_size = 300
     hidden_size = 300
-    num_layers = 1
+    num_layers = 2
+    bidirectional = True
+    dropout = 0.3
+    epochs = EPOCHS
+    # epochs = 1
 
     # model  vocab_size,hidden_dim,input_size,hidden_size,num_layers=2,bidirectional=True
-    model = Model(input_size = input_size,hidden_dim = hidden_size ,LSTM_layers = num_layers,vocab_size = vocab_size)
+    model = Model(vocab_size, hidden_dim, input_size, hidden_size, num_layers, bidirectional)
     model.load_state_dict(torch.load(MODEL_PATH))
     model.to(device)
 
@@ -343,5 +401,5 @@ def evaluate(text_list):
 
 # Main
 if __name__ == "__main__":
-    # train()
-    evaluate(["I love you","I want to hit someone","go to hill shit","I will kill you"])
+    train()
+    evaluate(["I love you","I want to hit someone"])
